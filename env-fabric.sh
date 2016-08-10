d5d00cfaf48e40614a7e446525b21cdbfb8c1ead
Helge Mathee <helge.mathee@fabricengine.com>
2016-08-10 15:28:31 +0200

    fix up for USD built on centos box

--- env-fabric.sh
+++ env-fabric.sh
@@ -1,16 +1,13 @@
-#OPT=/srv/chroots/usd-centos6/opt
 OPT=/opt
 USD_DEPS=$OPT/usd-deps/root/usr
-VFX_DEPS=$OPT/pixar/usd-vfx2015
-USD=$OPT/pixar/usd-deps/root/usr
-USD_PIXAR=$OPT/pixar/usd-deps/root/usr
+USD=$OPT/pixar/usd
+USD_SOURCE=$OPT/pixar/USD
 
 export PATH=$USD_DEPS/bin:$USD/bin:$PATH
-export LD_LIBRARY_PATH=$USD_DEPS/lib:$USD/lib:$VFX_DEPS/lib:$OPT/gcc-4.8/lib64:$USD_PIXAR/share/usd/fabric/plugin:$LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=$USD_DEPS/lib:$USD/lib:$OPT/gcc-4.8/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$USD_DEPS/lib:$USD/lib:$OPT/gcc-4.8/lib64:$USD/share/usd/fabric/plugin:$LD_LIBRARY_PATH
 export PYTHONPATH=$USD_DEPS/lib/python2.7:$USD/lib/python:/usr/local/lib/python/pxr:$PYTHONPATH
 export CPATH=$USD_DEPS/include
 
-export USD_CODEGEN_DIR=$OPT/USD/pxr/usd/lib/usd/codegenTemplates
+export USD_CODEGEN_DIR=$USD_SOURCE/pxr/usd/lib/usd/codegenTemplates
 
 scl enable devtoolset-2 bash
